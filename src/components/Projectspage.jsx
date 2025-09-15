cd1b5204b2119153ea4c7721ea96b43cca4687dd
pravesh yadav <129743521+praveshyadav753@users.noreply.github.com>
2025-09-15 19:56:24 +0530

    added project
    ai voice assistance

--- src/components/Projectspage.jsx
+++ src/components/Projectspage.jsx
@@ -39,7 +39,26 @@ const projectData = [
           linkTitle: "Github"
         }
       ]
-    }
+    },
+  {
+    name: "AI-Powered Desktop Voice Assistant",
+    technology: "Python, Google Gemini API, SpeechRecognition, vosk ,edge TTS, FastAPI, PyAutoGUI, threading/asyncio.",
+    bulletPoints: [
+      "Built a custom voice assistant with wake word detection ("Nesty") enabling real-time voice activation and interaction..",
+      "Integrated with Google Gemini AI, allowing the assistant to understand natural language, reason through complex tasks, and respond conversationally.",
+      "Implemented system-level automation, enabling voice control over mouse movements, keyboard input, application launching, and file managementImplemented system-level automation, enabling voice control over mouse movements, keyboard input, application launching, and file management",
+      "Developed a live web dashboard using react + websocket to display assistant status (listening, processing, speaking) in real-time.",
+      "Optimized performance through multithreading/asynchronous architecture, ensuring seamless execution without lag during concurrent tasks",
+      
+    ],
+    links: [
+      {
+        link: "https://github.com/praveshyadav753/AI-Assistent",
+        linkTitle: "Github"
+      },
+      
+    ]
+  },
   ];
 
 function Projectspage() {
